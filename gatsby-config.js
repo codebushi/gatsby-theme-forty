569a1418185a10ce54dd207a2a8ee8d6beb37b89
Hunter Chang <hunter@madwiremedia.com>
2019-07-25 16:42:06 -0600

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Theme - Forty',
+    author: 'Hunter Chang',
+    description: 'A Gatsby.js Theme based on Forty by HTML5 UP',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-theme-forty',
+        short_name: 'theme',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: path.join(__dirname, `src`, `pages`),
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
+  ],
+}
