acba3ffc3c8257854042e364e0c1200e4ce5856b
Hunter Chang <hunter@madwiremedia.com>
2019-07-30 16:38:19 -0600

    update a11y

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,27 @@
 import React from 'react'
 
-const Banner = (props) => (
-    <section id="banner" className="major">
-        <div className="inner">
-            <header className="major">
-                <h1>Hi, my name is Forty</h1>
-            </header>
-            <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
-                <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
-                </ul>
-            </div>
-        </div>
-    </section>
+const Banner = props => (
+  <section id="banner" className="major">
+    <div className="inner">
+      <header className="major">
+        <h1>Hi, my name is Forty</h1>
+      </header>
+      <div className="content">
+        <p>
+          A responsive site template designed by HTML5 UP
+          <br />
+          and released under the Creative Commons.
+        </p>
+        <ul className="actions">
+          <li>
+            <a href="#one" name="Get Started" className="button next scrolly">
+              Get Started
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 )
 
 export default Banner
